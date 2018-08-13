3b5d449431fe84d0da9afc8281ea85f26c49ce25
Mazid001 <39764711+Mazid001@users.noreply.github.com>
2018-08-13 09:36:16 +0600

    Add files via upload

---
+++ 13/sidebar.php
@@ -0,0 +1,15 @@
+
+<link rel="stylesheet" type="text/css" href="newstyle.css">
+
+<td width="190" valign="top">
+ <div class="sidebar template clear">
+   <ul>
+      <li><a href="home.php" >Insert Data</a></li>
+      <li><a href="viewdata.php" target ="iframe">View Data</a></li>
+      <li><a href="listdata.php" target ="iframe">List Data</a></li>
+      <li><a href="logoutm.php".php" target ="iframe">Logout</a></li>
+	  
+   </ul>
+  </div> 
+ </td>
+ <td valign="top">
